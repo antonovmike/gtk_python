0f00b9c67e5612eab350341c3d6e7d9be4973494
antonovmike <antonvma@outlook.com>
2023-01-14 12:37:13 +0500

    keyboard

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # gtk_python
 
+![python_gtk](https://github.com/antonovmike/gtk_python/blob/test/Screenshot.png)
+
 ```bash
 python -m pip install --upgrade pip
 sudo apt install libgirepository1.0-dev
--- main.py
+++ main.py
@@ -9,28 +9,20 @@ class GridWindow(Gtk.Window):
 
         super().__init__(title="Grid Example")
 
-        button1 = Gtk.Button(label="Button 1")
-        button2 = Gtk.Button(label="Button 2")
-        button3 = Gtk.Button(label="Button 3")
-        button4 = Gtk.Button(label="Button 4")
-        button5 = Gtk.Button(label="Button 5")
-        button6 = Gtk.Button(label="Button 6")
-        button7 = Gtk.Button(label="Button 7")
-        button8 = Gtk.Button(label="Button 8")
-        button9 = Gtk.Button(label="Button 9")
-        button0 = Gtk.Button(label="Button 0")
-
+        index = 0
         grid = Gtk.Grid()
-        grid.attach(button1, 0, 0, 1, 1)
-        grid.attach(button2, 1, 0, 1, 1)
-        grid.attach(button3, 2, 0, 1, 1)
-        grid.attach(button4, 0, 1, 1, 1)
-        grid.attach(button5, 1, 1, 1, 1)
-        grid.attach(button6, 2, 1, 1, 1)
-        grid.attach(button7, 0, 2, 1, 1)
-        grid.attach(button8, 1, 2, 1, 1)
-        grid.attach(button9, 2, 2, 1, 1)
 
+        while index < 9:
+            button = Gtk.Button(label=f"Button {index}")
+            if index < 3:
+                grid.attach(button, index, 0, 1, 1)
+            elif 2 < index < 6:
+                grid.attach(button, index-3, 1, 1, 1)
+            elif 5 < index < 9:
+                grid.attach(button, index-6, 2, 1, 1)
+            index += 1
+
+        button0 = Gtk.Button(label="Button 0")
         grid.attach(button0, 0, 3, 3, 1)
 
         self.add(grid)
