be5347a87d56899decb4899c80bcc4c1ae2b7106
Antonov Mike <101407994+antonovmike@users.noreply.github.com>
2023-01-16 14:18:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,36 @@ class GridWindow(Gtk.Window):
         index = 0
         grid = Gtk.Grid()
 
-        while index < 9:
-            button = Gtk.Button(label=f"Button {index}")
+        entry = Gtk.Entry()
+        grid.attach(entry, 0, 0, 3, 1)
+
+        while index < 10:
+            button = Gtk.Button(label=f"{index}")
             if index < 3:
-                grid.attach(button, index, 0, 1, 1)
+                grid.attach(button, index, 1, 1, 1)
             elif 2 < index < 6:
-                grid.attach(button, index-3, 1, 1, 1)
+                grid.attach(button, index-3, 2, 1, 1)
             elif 5 < index < 9:
-                grid.attach(button, index-6, 2, 1, 1)
+                grid.attach(button, index-6, 3, 1, 1)
             index += 1
 
-        button0 = Gtk.Button(label="Button 0")
-        grid.attach(button0, 0, 3, 3, 1)
+        button_dot = Gtk.Button(label=".")
+        button0 = Gtk.Button(label="0")
+        button_eq = Gtk.Button(label="=")
+        grid.attach(button_dot, 0, 4, 1, 1)
+        grid.attach(button0, 1, 4, 1, 1)
+        grid.attach(button_eq, 2, 4, 1, 1)
+
+        button_c = Gtk.Button(label="C")
+        button_plus = Gtk.Button(label="+")
+        button_minus = Gtk.Button(label="-")
+        button_mult = Gtk.Button(label="*")
+        button_div = Gtk.Button(label="/")
+        grid.attach(button_c, 4, 0, 1, 1)
+        grid.attach(button_plus, 4, 1, 1, 1)
+        grid.attach(button_minus, 4, 2, 1, 1)
+        grid.attach(button_mult, 4, 3, 1, 1)
+        grid.attach(button_div, 4, 4, 1, 1)
 
         self.add(grid)
 
