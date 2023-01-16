f4eac006a95357a464365fffc3157ee7a3b5be89
antonovmike <antonvma@outlook.com>
2023-01-16 14:01:30 +0500

    Notebook

--- main.py
+++ main.py
@@ -1,31 +1,78 @@
 import gi
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
 
 
-class MyWindow(Gtk.Window):
+class EntryWindow(Gtk.Window):
     def __init__(self):
-        super().__init__(title="Simple Notebook Example")
-        self.set_border_width(3)
+        super().__init__(title="Entry Demo")
+        self.set_size_request(200, 100)
 
-        self.notebook = Gtk.Notebook()
-        self.add(self.notebook)
+        self.timeout_id = None
 
-        self.page1 = Gtk.Box()
-        self.page1.set_border_width(10)
-        self.page1.add(Gtk.Label(label="Default Page!"))
-        self.notebook.append_page(self.page1, Gtk.Label(label="Plain Title"))
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
 
-        self.page2 = Gtk.Box()
-        self.page2.set_border_width(10)
-        self.page2.add(Gtk.Label(label="A page with an image for a Title."))
-        self.notebook.append_page(
-            self.page2, Gtk.Image.new_from_icon_name("help-about", Gtk.IconSize.MENU)
-        )
+        self.entry = Gtk.Entry()
+        self.entry.set_text("Hello World")
+        vbox.pack_start(self.entry, True, True, 0)
+
+        hbox = Gtk.Box(spacing=6)
+        vbox.pack_start(hbox, True, True, 0)
+
+        self.check_editable = Gtk.CheckButton(label="Editable")
+        self.check_editable.connect("toggled", self.on_editable_toggled)
+        self.check_editable.set_active(True)
+        hbox.pack_start(self.check_editable, True, True, 0)
+
+        self.check_visible = Gtk.CheckButton(label="Visible")
+        self.check_visible.connect("toggled", self.on_visible_toggled)
+        self.check_visible.set_active(True)
+        hbox.pack_start(self.check_visible, True, True, 0)
+
+        self.pulse = Gtk.CheckButton(label="Pulse")
+        self.pulse.connect("toggled", self.on_pulse_toggled)
+        self.pulse.set_active(False)
+        hbox.pack_start(self.pulse, True, True, 0)
+
+        self.icon = Gtk.CheckButton(label="Icon")
+        self.icon.connect("toggled", self.on_icon_toggled)
+        self.icon.set_active(False)
+        hbox.pack_start(self.icon, True, True, 0)
+
+    def on_editable_toggled(self, button):
+        value = button.get_active()
+        self.entry.set_editable(value)
+
+    def on_visible_toggled(self, button):
+        value = button.get_active()
+        self.entry.set_visibility(value)
+
+    def on_pulse_toggled(self, button):
+        if button.get_active():
+            self.entry.set_progress_pulse_step(0.2)
+            # Call self.do_pulse every 100 ms
+            self.timeout_id = GLib.timeout_add(100, self.do_pulse, None)
+        else:
+            # Don't call self.do_pulse anymore
+            GLib.source_remove(self.timeout_id)
+            self.timeout_id = None
+            self.entry.set_progress_pulse_step(0)
+
+    def do_pulse(self, user_data):
+        self.entry.progress_pulse()
+        return True
+
+    def on_icon_toggled(self, button):
+        if button.get_active():
+            icon_name = "system-search-symbolic"
+        else:
+            icon_name = None
+        self.entry.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY, icon_name)
 
 
-win = MyWindow()
+win = EntryWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
--- notebook.py
+++ notebook.py
@@ -14,12 +14,12 @@ class MyWindow(Gtk.Window):
 
         self.page1 = Gtk.Box()
         self.page1.set_border_width(10)
-        self.page1.add(Gtk.Label(label="Default Page!"))
+        self.page1.add(Gtk.Entry())
         self.notebook.append_page(self.page1, Gtk.Label(label="Plain Title"))
 
         self.page2 = Gtk.Box()
         self.page2.set_border_width(10)
-        self.page2.add(Gtk.Label(label="A page with an image for a Title."))
+        self.page2.add(Gtk.Entry())
         self.notebook.append_page(
             self.page2, Gtk.Image.new_from_icon_name("help-about", Gtk.IconSize.MENU)
         )
@@ -29,3 +29,4 @@ win = MyWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
+
